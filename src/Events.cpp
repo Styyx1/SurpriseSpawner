6bdc476dc6435502d8b44942fb971f5838201bbe
Styyx <styyx228@gmail.com>
2024-04-15 10:00:18 +0200

    shrink NPC for 1.5 sec to make it look better

--- src/Events.cpp
+++ src/Events.cpp
@@ -13,6 +13,10 @@ namespace Events
         auto dead_guy = event->objectActivated->As<RE::Actor>();
         auto settings = Settings::GetSingleton();
         auto util     = Utility::GetSingleton();
+   
+        const auto scriptFactory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::Script>();
+        const auto script        = scriptFactory ? scriptFactory->Create() : nullptr;
+  
 
         // Only do stuff when looking at dead actors
         if (dead_guy && dead_guy->IsDead()) {
@@ -21,12 +25,28 @@ namespace Events
             /* check for a random number, so
             1) it doesn't happen too often and
             2) it doesn't happen on the same npc twice in a row
-            (technically still possible but unlikely) */
+            (technically still possible but unlikely)
+            I still haven't figured out how to automatically close the menu
+            */
             if (chance == settings->compareValue) {
-                logger::debug("you looked at {} and the random int was {}", event->objectActivated->GetDisplayFullName(), std::to_string(chance));
+                logger::debug("you looked at {} and the random int was {}", event->objectActivated->GetDisplayFullName(), std::to_string(chance));              
                 dead_guy->AsReference()->PlaceObjectAtMe(settings->SpawnExplosion, false);
                 auto dude = dead_guy->AsReference()->PlaceObjectAtMe(settings->SpawnEnemy, false)->AsReference();
+                float scale = 0.001f;
+                script->SetCommand(fmt::format(FMT_STRING("setscale {}"), scale));
+                script->CompileAndRun(dude);
+                logger::debug("shrinked dude");
+                std::jthread([=] {
+                    std::this_thread::sleep_for(1.5s);
+                    SKSE::GetTaskInterface()->AddTask([=] {
+                        float size = 1.0f;
+                        script->SetCommand(fmt::format(FMT_STRING("setscale {}"), size));
+                        script->CompileAndRun(dude);
+                        logger::debug("made dude big");
+                    });
+                }).detach();
                 dude->Enable(false);
+                
             }
         }
         return RE::BSEventNotifyControl::kContinue;
